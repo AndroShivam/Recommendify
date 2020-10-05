9e8389b78bc2500f510c485bf7d0284e39184a45
AndroShivam <52043419+AndroShivam@users.noreply.github.com>
2020-10-05 12:11:15 +0530

    fixed issues
    fixed crashing when searching with no input, changes html input to django form , alot of improvements here and there

--- home/views.py
+++ home/views.py
@@ -2,27 +2,29 @@ import spotipy
 from django.shortcuts import render
 from spotipy.oauth2 import SpotifyClientCredentials
 from home.utils.api import SpotifyAPI
+from home.utils.form import SearchForm
 
-client_id = 'your_client_id'
+client_id  = 'your_client_id'
 client_secret = 'your_client_secret'
 
 ccm = spotipy.SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
 sp = spotipy.Spotify(client_credentials_manager = ccm)
 
+spotify = SpotifyAPI(client_id = client_id, client_secret = client_secret)
 
 
 def main_home(request):
+    result = None
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
 
-    query_to_be_search = request.POST.get('search', None)
-
-    if query_to_be_search is None or "":
-        return render(request, 'home/home.html', {'result' : None})
-
+        if form.is_valid():
+            query_to_be_search = form.cleaned_data['search']
+            query_result = spotify.search(query = query_to_be_search)
+            items = query_result['artists']['items']
+            artist_uri = items[0]['uri']
+            result = sp.artist_related_artists(artist_uri)
     else:
-        spotify = SpotifyAPI(client_id = client_id, client_secret = client_secret)
-        query_result = spotify.search(query = query_to_be_search)
-        items = query_result['artists']['items']
-        artist_uri = items[0]['uri']
-        result = sp.artist_related_artists(artist_uri)
+        form = SearchForm()
 
-    return render(request, 'home/home.html', {'result' : result})
+    return render(request, 'home/home.html', {'form' : form, 'result' : result})
